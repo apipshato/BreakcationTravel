1b642947ee1b30b3fb47c81e54f0abc54ed4ddf0
apipshato <afifshatos@gmail.com>
2020-06-17 11:10:56 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import from 'assets/scss/style.scss';
+
+function App(){
+    return (
+
+        <div className="App">
+            hai dunia!!
+        </div>
+    )
+}
