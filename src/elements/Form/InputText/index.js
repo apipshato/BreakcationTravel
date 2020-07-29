5dafafbe4cf9816f1c4eacde8dd419bfd15d34e8
apipshato <afifshatos@gmail.com>
2020-07-30 00:11:03 +0700

    make form input text and fill proptypes

--- src/elements/Form/InputText/index.js
+++ src/elements/Form/InputText/index.js
@@ -21,5 +21,8 @@ Text.propTypes={
     value: propTypes.oneOfType([propTypes.number, propTypes.string]).isRequired,
     onChange:propTypes.func.isRequired,
     prepend: propTypes.oneOfType([propTypes.number, propTypes.string]),
-    
+    append: propTypes.oneOfType([propTypes.number, propTypes.string]),
+    type:propTypes.string,
+    outerClassname:propTypes.string,
+    inputClassname:propTypes.string
 }
